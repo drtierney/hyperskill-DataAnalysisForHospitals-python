36b7fefcb65f6834d58b9db56f6a424aafcac28a
drtierney <d.r.tierney@hotmail.co.uk>
2021-06-20 14:25:48 +0100

    Stage #3 completed
    Stage 3/5: Improve your dataset

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 general_df = pd.read_csv(r"test\\general.csv")
 prenatal_df = pd.read_csv(r"test\\prenatal.csv")
@@ -10,5 +10,11 @@ sports_df.rename(columns={"Hospital": "hospital", "Male/female": "gender"}, inpl
 
 merged_df = pd.concat([general_df, prenatal_df, sports_df], ignore_index=True)
 merged_df.drop(columns=["Unnamed: 0"], inplace=True)
+merged_df.dropna(how="all", inplace=True)
+
+merged_df["gender"] = merged_df["gender"].replace(["man", "male"], "m")
+merged_df["gender"] = merged_df["gender"].replace(["woman", "female", np.nan], "f")
+
+merged_df.fillna(0, inplace=True)
 
 print(merged_df.head(20))
--- Data Analysis for Hospitals/task/tests.py
+++ Data Analysis for Hospitals/task/tests.py
@@ -2,27 +2,27 @@ from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
 from hstest.check_result import CheckResult
 
-df_result = '''   hospital gender   age  height  weight  ...  ultrasound  mri xray children months
-0   general    man  33.0    1.64    66.0  ...         NaN  NaN  NaN      NaN    NaN
-1   general    man  48.0    1.93   106.0  ...           t  NaN  NaN      NaN    NaN
-2   general  woman  23.0    1.54    63.0  ...         NaN  NaN  NaN      NaN    NaN
-3   general    man  27.0    1.94   103.0  ...           t  NaN  NaN      NaN    NaN
-4   general  woman  22.0    1.76    74.0  ...         NaN  NaN    f      NaN    NaN
-5   general    man  46.0    1.90    96.0  ...         NaN  NaN  NaN      NaN    NaN
-6   general  woman  68.0    1.80    85.0  ...           f  NaN  NaN      NaN    NaN
-7   general    man  35.0    1.57    61.0  ...         NaN  NaN  NaN      NaN    NaN
-8   general  woman  50.0    1.86    86.0  ...         NaN  NaN    f      NaN    NaN
-9   general    man  25.0    1.79    82.0  ...         NaN  NaN  NaN      NaN    NaN
-10  general    man  27.0    1.85    86.0  ...         NaN  NaN  NaN      NaN    NaN
-11  general    man  57.0    1.86    95.0  ...           f  NaN  NaN      NaN    NaN
-12  general    man  29.0    1.88   100.0  ...         NaN  NaN    f      NaN    NaN
-13  general  woman  18.0    1.70    80.0  ...         NaN  NaN  NaN      NaN    NaN
-14  general  woman  47.0    1.80    83.0  ...           f  NaN  NaN      NaN    NaN
-15  general  woman  51.0    1.87   103.0  ...         NaN  NaN  NaN      NaN    NaN
-16  general  woman  56.0    1.56    57.0  ...         NaN  NaN    t      NaN    NaN
-17  general  woman  38.0    1.62    71.0  ...         NaN  NaN  NaN      NaN    NaN
-18  general  woman  32.0    1.72    77.0  ...         NaN  NaN  NaN      NaN    NaN
-19  general  woman  69.0    1.72    80.0  ...         NaN  NaN    f      NaN    NaN
+df_result = '''   hospital gender   age  height  weight  ...  ultrasound mri xray children months
+0   general      m  33.0    1.64    66.0  ...           0   0    0      0.0    0.0
+1   general      m  48.0    1.93   106.0  ...           t   0    0      0.0    0.0
+2   general      f  23.0    1.54    63.0  ...           0   0    0      0.0    0.0
+3   general      m  27.0    1.94   103.0  ...           t   0    0      0.0    0.0
+4   general      f  22.0    1.76    74.0  ...           0   0    f      0.0    0.0
+5   general      m  46.0    1.90    96.0  ...           0   0    0      0.0    0.0
+6   general      f  68.0    1.80    85.0  ...           f   0    0      0.0    0.0
+7   general      m  35.0    1.57    61.0  ...           0   0    0      0.0    0.0
+8   general      f  50.0    1.86    86.0  ...           0   0    f      0.0    0.0
+9   general      m  25.0    1.79    82.0  ...           0   0    0      0.0    0.0
+10  general      m  27.0    1.85    86.0  ...           0   0    0      0.0    0.0
+11  general      m  57.0    1.86    95.0  ...           f   0    0      0.0    0.0
+12  general      m  29.0    1.88   100.0  ...           0   0    f      0.0    0.0
+13  general      f  18.0    1.70    80.0  ...           0   0    0      0.0    0.0
+14  general      f  47.0    1.80    83.0  ...           f   0    0      0.0    0.0
+15  general      f  51.0    1.87   103.0  ...           0   0    0      0.0    0.0
+16  general      f  56.0    1.56    57.0  ...           0   0    t      0.0    0.0
+17  general      f  38.0    1.62    71.0  ...           0   0    0      0.0    0.0
+18  general      f  32.0    1.72    77.0  ...           0   0    0      0.0    0.0
+19  general      f  69.0    1.72    80.0  ...           0   0    f      0.0    0.0
 
 [20 rows x 14 columns]'''
 
@@ -36,17 +36,11 @@ class EDATest(StageTest):
         lines_with_digit = [i for i in lines if len(i) > 0 and i[0].isdigit()]
         columns = lines[0].split(' ')
         if 'Unnamed: 0' in columns:
-            return CheckResult.wrong(
-                'Holy-moly! you\'ve printed \'Unnamed: 0\' column')
-
+            return CheckResult.wrong(feedback='Holy-moly! you\'ve printed \'Unnamed: 0\' column')
         if len(lines_with_digit) != 20:
-            return CheckResult.wrong(
-                'There should be 20 lines of data, found ' + str(len(lines_with_digit)))
-
+            return CheckResult.wrong(feedback='There should be 20 lines of data, found ' + str(len(lines_with_digit)))
         if df_result not in reply:
-            return CheckResult.wrong(
-                "Seems like your answer is incorrect")
-
+            return CheckResult.wrong(feedback="Seems like your answer is incorrect. Look at the example")
         return CheckResult.correct()
 
 
