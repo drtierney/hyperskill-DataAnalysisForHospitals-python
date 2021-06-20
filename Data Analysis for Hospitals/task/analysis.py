e35c19b2d6a2e03379cc4afbad1f414ed68f753e
drtierney <d.r.tierney@hotmail.co.uk>
2021-06-20 15:41:57 +0100

    Stage #4 completed
    Stage 4/5: The statistics

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -17,4 +17,25 @@ merged_df["gender"] = merged_df["gender"].replace(["woman", "female", np.nan], "
 
 merged_df.fillna(0, inplace=True)
 
-print(merged_df.head(20))
+# Which hospital has the highest number of patients?
+q1_df = merged_df["hospital"]
+q1_answer = q1_df.value_counts().keys().tolist()[0]
+print(f"The answer to the 1st question is {q1_answer}")
+
+# What share of the patients in the general hospital suffers from stomach-related issues?
+# Round the result to the third decimal place.
+q2_df = merged_df[merged_df["hospital"] == "general"]
+q2_answer = len(q2_df.query('diagnosis == "stomach"')) / len(q2_df)
+print(f"The answer to the 2nd question is {round(q2_answer, 3)}")
+
+# What share of the patients in the sports hospital suffers from dislocation-related issues?
+# Round the result to the third decimal place.
+q3_df = merged_df.query('hospital == "sports"')
+q3_answer = len(q3_df.loc[q3_df["diagnosis"] == "dislocation"]) / len(q3_df)
+print(f"The answer to the 3rd question is {round(q3_answer, 3)}")
+
+# What is the difference in the median ages of the patients in the general and sports hospitals?
+q4_gen_median = merged_df.query('hospital == "general"').age.median()
+q4_spo_median = merged_df[merged_df["hospital"] == "sports"]["age"].median()
+q4_answer = max(q4_gen_median, q4_spo_median) - min(q4_gen_median, q4_spo_median)
+print(f"The answer to the 4th question is {q4_answer}")
--- Data Analysis for Hospitals/task/tests.py
+++ Data Analysis for Hospitals/task/tests.py
@@ -2,29 +2,7 @@ from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
 from hstest.check_result import CheckResult
 
-df_result = '''   hospital gender   age  height  weight  ...  ultrasound mri xray children months
-0   general      m  33.0    1.64    66.0  ...           0   0    0      0.0    0.0
-1   general      m  48.0    1.93   106.0  ...           t   0    0      0.0    0.0
-2   general      f  23.0    1.54    63.0  ...           0   0    0      0.0    0.0
-3   general      m  27.0    1.94   103.0  ...           t   0    0      0.0    0.0
-4   general      f  22.0    1.76    74.0  ...           0   0    f      0.0    0.0
-5   general      m  46.0    1.90    96.0  ...           0   0    0      0.0    0.0
-6   general      f  68.0    1.80    85.0  ...           f   0    0      0.0    0.0
-7   general      m  35.0    1.57    61.0  ...           0   0    0      0.0    0.0
-8   general      f  50.0    1.86    86.0  ...           0   0    f      0.0    0.0
-9   general      m  25.0    1.79    82.0  ...           0   0    0      0.0    0.0
-10  general      m  27.0    1.85    86.0  ...           0   0    0      0.0    0.0
-11  general      m  57.0    1.86    95.0  ...           f   0    0      0.0    0.0
-12  general      m  29.0    1.88   100.0  ...           0   0    f      0.0    0.0
-13  general      f  18.0    1.70    80.0  ...           0   0    0      0.0    0.0
-14  general      f  47.0    1.80    83.0  ...           f   0    0      0.0    0.0
-15  general      f  51.0    1.87   103.0  ...           0   0    0      0.0    0.0
-16  general      f  56.0    1.56    57.0  ...           0   0    t      0.0    0.0
-17  general      f  38.0    1.62    71.0  ...           0   0    0      0.0    0.0
-18  general      f  32.0    1.72    77.0  ...           0   0    0      0.0    0.0
-19  general      f  69.0    1.72    80.0  ...           0   0    f      0.0    0.0
-
-[20 rows x 14 columns]'''
+answers = ['general', '0.325', '0.285', '19.0']
 
 
 class EDATest(StageTest):
@@ -32,15 +10,25 @@ class EDATest(StageTest):
         return [TestCase()]
 
     def check(self, reply, attach):
-        lines = reply.split('\n')
-        lines_with_digit = [i for i in lines if len(i) > 0 and i[0].isdigit()]
-        columns = lines[0].split(' ')
-        if 'Unnamed: 0' in columns:
-            return CheckResult.wrong(feedback='Holy-moly! you\'ve printed \'Unnamed: 0\' column')
-        if len(lines_with_digit) != 20:
-            return CheckResult.wrong(feedback='There should be 20 lines of data, found ' + str(len(lines_with_digit)))
-        if df_result not in reply:
-            return CheckResult.wrong(feedback="Seems like your answer is incorrect. Look at the example")
+        lines = [line for line in reply.split('\n') if len(line) > 0]
+
+        if len(lines) != 4:
+            return CheckResult.wrong(
+                'You should output exactly 4 lines with answer to each question in each line. '
+                f'Found {len(lines)} lines')
+
+        if answers[0] not in lines[0]:
+            return CheckResult.wrong('The answer to the 1st question is incorrect')
+
+        if answers[1] not in lines[1]:
+            return CheckResult.wrong('The answer to the 2nd question is incorrect')
+
+        if answers[2] not in lines[2]:
+            return CheckResult.wrong('The answer to the 3rd question is incorrect')
+
+        if answers[3] not in lines[3]:
+            return CheckResult.wrong('The answer to the 4th question is incorrect')
+
         return CheckResult.correct()
 
 
